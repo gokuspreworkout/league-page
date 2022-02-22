ebfa4b5b8d541615eac66a890ba0dcb380b3affa
gokuspreworkout <100219960+gokuspreworkout@users.noreply.github.com>
2022-02-22 16:45:38 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -164,7 +164,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "California", // (optional)
       "bio": "Professional Tight End Hater. What Kevin lacks in fantasy prowess, he more than makes up for in personality - Gokuspreworkout",
-      "photo": "/managers/tylermonge.PNG", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/ow.PNG", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
