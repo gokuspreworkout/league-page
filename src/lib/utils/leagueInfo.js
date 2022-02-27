175f56e6704744380911cdab31e3d276a248a7f2
gokuspreworkout <100219960+gokuspreworkout@users.noreply.github.com>
2022-02-27 16:17:25 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -188,7 +188,7 @@ export const managers = [
       "bio": "Professional Tight End Hater. What Kevin lacks in fantasy prowess, he more than makes up for in personality - Gokuspreworkout",
       "photo": "/managers/ow.PNG", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "cin", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Jxnw", // Can be anything (usually your rival's name)
